147c0a1c4d271ff91ccccc3a752ff34e5b7e5c8f
Dong Young Yoon <dongyoungy@gmail.com>
2018-10-09 18:38:58 -0400

    bug fix

--- src/main/java/dyoon/Cache.java
+++ src/main/java/dyoon/Cache.java
@@ -5,17 +5,17 @@ import java.util.*;
 
 /**
  * Created by Dong Young Yoon on 3/20/18.
- * <p>
- * A serializable, singleton cache class for store various statistics
- * that could take a long time to obtain into a file.
+ *
+ * <p>A serializable, singleton cache class for store various statistics that could take a long time
+ * to obtain into a file.
  */
 public class Cache {
 
-  public final static String TABLE_SIZE_SUFFIX = ".tablesize";
-  public final static String GROUP_COUNT_SUFFIX = ".groupcount";
-  public final static String AVG_GROUP_SIZE_SUFFIX = ".avggroupsize";
+  public static final String TABLE_SIZE_SUFFIX = ".tablesize";
+  public static final String GROUP_COUNT_SUFFIX = ".groupcount";
+  public static final String AVG_GROUP_SIZE_SUFFIX = ".avggroupsize";
 
-  private final static String CACHE_FILE_PATH = "." + File.separator + "cache";
+  private static final String CACHE_FILE_PATH = "." + File.separator + "cache";
 
   private static Cache singleInstance = null;
   private Map<String, Object> cache;
@@ -28,10 +28,8 @@ public class Cache {
     if (cacheFile.exists()) {
       if (cacheFile.isDirectory()) {
         System.err.println(
-            String.format("A cache %s cannot be accessed: it is a directory.",
-                CACHE_FILE_PATH));
-      }
-      else {
+            String.format("A cache %s cannot be accessed: it is a directory.", CACHE_FILE_PATH));
+      } else {
         if (!initialize()) {
           System.err.println("Failed to initialize cache");
         }
@@ -123,23 +121,24 @@ public class Cache {
   }
 
   public Long getGroupCount(Query q) {
-    String key = String.format("%s_%s", q.getQCSString(), GROUP_COUNT_SUFFIX);
+    String key = String.format("%s_%s_%s", q.getFactTable(), q.getQCSString(), GROUP_COUNT_SUFFIX);
     return (Long) this.get(key);
   }
 
   public void setGroupCount(Query q, Long value) {
-    String key = String.format("%s_%s", q.getQCSString(), GROUP_COUNT_SUFFIX);
+    String key = String.format("%s_%s_%s", q.getFactTable(), q.getQCSString(), GROUP_COUNT_SUFFIX);
     this.put(key, value);
   }
 
   public Double getAverageGroupSize(Query q) {
-    String key = String.format("%s_%s", q.getQCSString(), AVG_GROUP_SIZE_SUFFIX);
+    String key =
+        String.format("%s_%s_%s", q.getFactTable(), q.getQCSString(), AVG_GROUP_SIZE_SUFFIX);
     return (Double) this.get(key);
   }
 
   public void setAverageGroupSize(Query q, Double value) {
-    String key = String.format("%s_%s", q.getQCSString(), AVG_GROUP_SIZE_SUFFIX);
+    String key =
+        String.format("%s_%s_%s", q.getFactTable(), q.getQCSString(), AVG_GROUP_SIZE_SUFFIX);
     this.put(key, value);
   }
 }
-
